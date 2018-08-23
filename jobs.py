3b56c178a3c4766d332310fe26dcb308bd92bbfe
xavier <xaviersulkowski@gmail.com>
2018-08-24 00:47:52 +0200

    Jobs module prepared

---
+++ jobs.py
@@ -0,0 +1,5 @@
+from workers.config_parser import Parser
+from workers.abstract_worker import Worker
+from workers.free_memory import FreeMemory
+from workers.free_space import FreeSpace
+from workers.ping import Ping
--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-from workers.config_parser import Parser
-from workers.abstract_worker import Worker
-from workers.free_memory import FreeMemory
-from workers.free_space import FreeSpace
-from workers.ping import Ping
 
